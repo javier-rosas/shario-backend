e9bde0e2107f107c1a7cb144cbf88ae1b9042025
javier-rosas <jr2dj@virginia.edu>
2022-10-10 20:38:04 -0400

    finished auth js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,19 @@
 const jwt = require("jsonwebtoken")
 
-const checkUser = () => {
+const checkUser = (req, res, next) => {
   const bearerHeader = req.headers['authorization']
   if (typeof bearerHeader !== 'undefined') {
     req.token = bearerHeader
     try {
-
+      const decoded = jwt.verify(req.token, process.env.JWT_SECRET)
+      req.user = decoded
+      next()
     } catch(e) {
-
+      console.log(e)
+      return res.status(401).send('Unauthorized')
     }
+  } else {
+    return res.status(401).send('Unauthorized');
   }
 }
 
